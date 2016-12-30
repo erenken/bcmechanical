f97ed106aa48e3071632449154e8f6621d9a8f52
Eric Renken <ericr@exaktime.com>
2016-12-29 23:42:06 -0500

    start of new version with vs2017 and angular2

---
+++ app/app.module.ts
@@ -0,0 +1,13 @@
+﻿import { NgModule } from "@angular/core";
+import { BrowserModule } from "@angular/platform-browser";
+
+import { NavComponent } from "./nav/component";
+import { HomeComponent } from "./home/component";
+
+@NgModule({
+	imports: [BrowserModule],
+	declarations: [HomeComponent, NavComponent],
+	bootstrap: [HomeComponent]
+})
+
+export class AppModule { }
